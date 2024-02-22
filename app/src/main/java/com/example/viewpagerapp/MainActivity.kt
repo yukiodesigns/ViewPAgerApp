8de9ae2eb1ad1858a37e1a18756532dd951ff726
Yukio <elsie.atieno@student.moringaschool.com>
2024-02-22 09:19:34 +0300

    Added activity

--- app/src/main/java/com/example/viewpagerapp/MainActivity.kt
+++ app/src/main/java/com/example/viewpagerapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.viewpagerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.example.viewpagerapp.Adapter.MyAdapter
 
 class MainActivity : AppCompatActivity() {
+    lateinit var myAdapter: MyAdapter
+    lateinit var viewPager: ViewPager2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.viewPager)
+        myAdapter = MyAdapter(this)
+        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        viewPager.adapter = myAdapter
+
+
+
     }
 }
